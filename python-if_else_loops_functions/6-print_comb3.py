e5e6a10ec95216cb8e78534bf642f83b10018b38
Larry-Sentore <l.sentore@alustudent.com>
2025-02-22 05:43:54 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(8):
-    for x in range (i+1, 10):
-        print("{}{}, ".format(i,x), end='')
+    for x in range(i+1, 10):
+        print("{}{}, ".format(i, x), end='')
 print("89")
