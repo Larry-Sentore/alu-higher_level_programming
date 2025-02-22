847674eae38fad74bda7b8a960b95c9faa69ee45
Larry-Sentore <l.sentore@alustudent.com>
2025-02-22 16:50:50 +0200

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = abs(number) % 10
-    last = ord(str(last))
+    lastd = abs(number) % 10
+    print(lastd)
+    last = ord(str(lastd))
     return last
