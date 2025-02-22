59ce466aa00ce360a293708720eb0e946d9d87b5
Larry-Sentore <l.sentore@alustudent.com>
2025-02-22 16:04:07 +0200

    7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if 97 <= ord(c) <= 122:
-        print("{} is lower".format(c))
-    else:
-        print("{} is upper".format(c))
+    return 97 <= ord(c) <= 122
