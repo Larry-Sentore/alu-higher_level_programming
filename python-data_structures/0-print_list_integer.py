da904ded713dcbe749dceab343e407963bc4a2c8
Larry-Sentore <l.sentore@alustudent.com>
2025-03-03 15:15:52 +0200

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list(my_list=[]):
     for i in my_list:
-        print("{}{:d}".format(i))
+        print("{i}".format(i))
+
+lol = [1,2,3,4,5]
+print(print_list(lol))
