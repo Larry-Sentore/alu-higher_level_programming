fb293e5079b7383f36864bb2054be2df42bac539
Larry-Sentore <l.sentore@alustudent.com>
2025-03-03 21:12:44 +0200

    yes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     for row in matrix:
+    for row in matrix:
         for i, num in enumerate(row):
             if i != 0:
                 print(" ", end="")
