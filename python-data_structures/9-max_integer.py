fb9b8100435d9907e15ce00e163504fa5c19ac3a
Larry-Sentore <l.sentore@alustudent.com>
2025-03-04 16:52:18 +0200

    max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxi = 0
+    maxi = -200
+    if not my_list:
+        return None
     for i in my_list:
         if i > maxi:
             maxi = i
