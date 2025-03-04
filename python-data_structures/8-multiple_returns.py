25d65067176e6971b318f7428285e7063ac83db0
Larry-Sentore <l.sentore@alustudent.com>
2025-03-04 16:44:16 +0200

    8-multiple_returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return None
+    if not sentence:
+        return (0, None)
     length = len(sentence)
     for char in sentence:
         first = char
