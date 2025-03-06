30afae320c7f33887322b43ab2c544b311bbfa41
Larry-Sentore <l.sentore@alustudent.com>
2025-03-06 15:13:14 +0200

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define the square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Creating this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
