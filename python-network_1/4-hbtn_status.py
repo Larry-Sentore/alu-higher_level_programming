edfd392ef5929dc7e9b8ba44f743c142de092d2f
Larry-Sentore <l.sentore@alustudent.com>
2025-04-05 18:36:52 +0200

    yes

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
 """
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
+import requests
 
-
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
-    try:
-        with request.urlopen(sys.argv[1]) as resq:
-            print(resq.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print('Error code:', e.code)
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
